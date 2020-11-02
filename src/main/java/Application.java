df418eb8a5568814b0a88dd40a4f3d163cb4e4e4
Tuba DEMIR <tubademir23@gmail.com>
2020-11-02 22:17:16 +0300

    predict test data with samples not in train data

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -13,17 +13,26 @@ public class Application {
 		Dataset<Row> raw_data = sparkSession.read().format("csv")
 				.option("header", "true").option("inferSchema", "true")
 				.load(".\\src\\data\\sales.csv");
+
+		Dataset<Row> new_data = sparkSession.read().format("csv")
+				.option("header", "true").option("inferSchema", "true")
+				.load(".\\src\\data\\test.csv");
+
 		raw_data.show();
 		VectorAssembler features_vector = new VectorAssembler()
 				.setInputCols(new String[]{"Ay"}).setOutputCol("features");
 
 		Dataset<Row> transform = features_vector.transform(raw_data);
 
+		Dataset<Row> transfor_new_data = features_vector.transform(new_data);
+
 		Dataset<Row> final_data = transform.select("features", "Satis");
 
 		Dataset<Row>[] datasets = final_data
-				.randomSplit(new double[]{0.7, 0.3});
+				.randomSplit(new double[]{0.9, 0.1});
 
+		// 0.7, 0.3 -> 23487.517
+		// 0.9, 0.1 -> 25406.119
 		Dataset<Row> train_data = datasets[0];
 		Dataset<Row> test_data = datasets[1];
 
@@ -32,7 +41,10 @@ public class Application {
 		LinearRegressionModel model = lr.fit(train_data);
 
 		Dataset<Row> transform_test = model.transform(test_data);
-		transform_test.show();
+		// transform_test.show();
+
+		Dataset<Row> transform_new_test = model.transform(transfor_new_data);
+		transform_new_test.show();
 
 	}
 
